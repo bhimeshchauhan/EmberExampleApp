2c4de1d00710a3e8b4c3c49c2a716b66c9a7b962
bchauhan <bchauhan@iastate.edu>
2018-03-16 01:48:55 -0500

    search by typing into search field

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL fillIn, triggerKeyEvent } from '@ember/test-helpers';
 
 
 module('Acceptance | my acceptance test', function(hooks) {
@@ -26,6 +26,14 @@ module('Acceptance | my acceptance test', function(hooks) {
   test('should list available rentals.', async function(assert) {
     await visit('/');
     assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.ok(this.element.querySelector('.results .listing'), 'should display 1 listing');
+    assert.ok(this.element.querySelector('.listing .location').textContent.includes('Seattle'), 'should contain 1 listing with location Seattle');
   });
 
 });
